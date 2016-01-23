560499f45a37994aff460ded46b7162b1689f893
josdem <joseluis.delacruz@gmail.com>
2016-01-23 14:00:37 -0600

    Adding functions example

---
+++ files/functions.js
@@ -0,0 +1,20 @@
+var countingRedCards = function(parkingLot){
+  var counter = 0
+
+  parkingLot.forEach(function(car){
+    if(car.color === 'Red'){
+      counter++
+    }
+  })
+
+  return counter
+}
+
+var parkingLot = [
+  {color:'Blue'},
+  {color:'Red'},
+  {color:'White'},
+  {color:'Red'}
+]
+
+console.log('redCars: ' + countingRedCards(parkingLot))
