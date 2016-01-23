68461ea574d8dd54a0635df1b003ed4733d05758
josdem <joseluis.delacruz@gmail.com>
2016-01-23 13:37:44 -0600

    Adding balance and counter

--- js/forEach.js
+++ js/forEach.js
@@ -1,8 +1,20 @@
 var numbers = [12.10, 13.50, 20.00]
 
 var total = 0
+var counter = 0
+var balance = 200.00
+
 numbers.forEach(function(price){
   total += price
+  counter += 1
 })
+balance -= total
 
+if(balance >= total){
+  console.log("Se realizo la compra!")
+}
+
+console.log('counter:', counter)
 console.log('total:', total)
+console.log('balance:', balance)
+
