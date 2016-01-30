ff9dd9964b9d038e6132dac6b06b6911189a241b
josdem <joseluis.delacruz@gmail.com>
2016-01-30 13:16:24 -0600

    Agregando el ejemplo con rendering

--- clase2/server.js
+++ clase2/server.js
@@ -1,9 +1,20 @@
 var express = require('express')
+var swig = require('swig')
 
 var application = express()
 
+//Swig configuration
+application.engine('html', swig.renderFile)
+application.set('view engine', 'html')
+application.set('views', __dirname + '/views')
+
+//Cache configuration
+// ToDo: Change cache:true in production
+application.set('view cache', false)
+swig.setDefaults({cache:false})
+
 application.get("/",function(request, response){
-  response.send("Hello World!")
+  response.render("index", {title: "Esta es una app"})
 })
 
 application.listen(3000, function(){
