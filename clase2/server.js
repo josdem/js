2910a184b9ad64711415681ffb89489e1de993de
josdem <joseluis.delacruz@gmail.com>
2016-01-30 13:33:50 -0600

    Agregando en el render el traer de la base de datos los todos

--- clase2/server.js
+++ clase2/server.js
@@ -1,21 +1,42 @@
 var express = require('express')
 var swig = require('swig')
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+mongoose.connect('mongodb://localhost/todo')
+
+var toDoSchema = new Schema({
+  name: String,
+  description: String
+})
+
+var ToDo = mongoose.model('ToDo', toDoSchema)
 
 var application = express()
 
-//Swig configuration
-application.engine('html', swig.renderFile)
-application.set('view engine', 'html')
-application.set('views', __dirname + '/views')
+  //Swig configuration
+  application.engine('html', swig.renderFile)
+  application.set('view engine', 'html')
+  application.set('views', __dirname + '/views')
 
-//Cache configuration
-// ToDo: Change cache:true in production
-application.set('view cache', false)
-swig.setDefaults({cache:false})
+  //Cache configuration
+  // ToDo: Change cache:true in production
+  application.set('view cache', false)
+  swig.setDefaults({cache:false})
 
-application.get("/",function(request, response){
-  response.render("index", {title: "Esta es una app"})
-})
+  application.get("/",function(request, response){
+
+    ToDo.find({}, function(err, docs){
+      if(err){
+        return response.send(500, 'Internal Server Error')
+      }
+
+      response.render("index", {
+        title: "Esta es una app",
+        toDos: docs
+      })
+    })
+  })
 
 application.listen(3000, function(){
   console.log('Server ready and listening at: 3000')
