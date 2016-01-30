27c435f86552a1d346120266aad8524916a7b23d
josdem <joseluis.delacruz@gmail.com>
2016-01-30 12:09:28 -0600

    Agregando ejemplo de listado de todos

--- clase2/list-todos.js
+++ clase2/list-todos.js
@@ -11,5 +11,15 @@ var toDoSchema = new Schema({
 var ToDo = mongoose.model('ToDo', toDoSchema)
 
 ToDo.find({}, function(err, docs){
+  if(err){
+    console.log('Hubo error', err)
+    return
+  }
+
+  console.log('Hay', docs.length, 'todos')
+
+  docs.forEach(function(toDo){
+    console.log('=>', toDo.name)
+  })
 
 })
