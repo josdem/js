bd14d39282454d1f126b7b4a819db28e2e2c9acc
josdem <joseluis.delacruz@gmail.com>
2016-03-21 09:35:33 -0600

    Agreando ejemplo de arrays

---
+++ arrays.js
@@ -0,0 +1,18 @@
+var numbers = [5,3,7,9,1]
+
+numbers.forEach(function(it){
+  if(it%3 == 0){
+    console.log(it)
+  }
+})
+
+var names = ["Danna", "Gael", "Jose Luis", "Teodoro", "Ruth"]
+
+names.forEach(function(name){
+  if(name.length == 4){
+    console.log(name)
+  }
+})
+
+
+
